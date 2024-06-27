a7378cc7a3b70c70c7b7c115d24d69d5cb03d78d
dawothepony <dawogamespl@gmail.com>
2024-06-28 01:51:51 +0200

    Fix for screen size detection

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import { About } from "./Component/About"
 function App() {
   return (
     <>
-    <div className=" bg-[#fff3ea] md:hidden lg:block sm:hidden ssm:hidden">
+    <div className=" bg-[#fff3ea] md:block lg:block sm:hidden ssm:hidden">
       <Nav />
       <Hero />
       <About />
     </div>
-    <div className=" sm:flex ssm:flex md:flex lg:hidden h-screen w-screen items-center justify-center text-5xl">
+    <div className=" sm:flex ssm:flex md:hidden lg:hidden h-screen w-screen items-center justify-center text-5xl">
       <p>
         Mobile coming soon!
       </p>
